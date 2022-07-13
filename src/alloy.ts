6748075fe0fabe1f5dd3fa59e505c5ce9f7035cd
Graham Rogers <grahamr@adobe.com>
2022-07-13 12:31:56 -0600

    working examples

--- src/alloy.ts
+++ src/alloy.ts
@@ -1,31 +1,30 @@
 import { createInstance } from '@adobe/alloy';
-import { AlloyInstance } from "./interfaces/alloy.types";
+import { AlloyInstance } from './interfaces/alloy.types';
+
+const alloyInstance: AlloyInstance = createInstance({ name: 'alloy' });
 
 
-const alloyInstance: AlloyInstance = createInstance({ name: "alloy" });
-
-// create alloy instance
-// configure alloy instance
-// send alloy event
+export const getInstance = () => {
+    return alloyInstance;
+}
 
 /**
  * sends event payload that matches the BeaconSchema that's been defined
  */
- export const sendEvent = async (schema: any): Promise<void> => {
-    try {
-        // const instance = await alloyInstance();
+export const sendEvent = async (schema: any): Promise<void> => {
+  try {
+    // const instance = await alloyInstance();
 
-        // // attach identity field
-        // const result: AlloyIndentity = (await instance("getIdentity")) as AlloyIndentity;
-        // schema.personID = result.identity.ECID || "unknown";
+    // // attach identity field
+    // const result: AlloyIndentity = (await instance("getIdentity")) as AlloyIndentity;
+    // schema.personID = result.identity.ECID || "unknown";
 
-        const xdm = { xdm: { ...schema } };
+    const xdm = { xdm: { ...schema } };
 
-        // send async
-        alloyInstance("sendEvent", xdm);
-    } catch (error) {
-        // eslint-disable-next-line no-console
-        console.error("sendEvent error:", error);
-    }
+    // send async
+    alloyInstance('sendEvent', xdm);
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('sendEvent error:', error);
+  }
 };
-
--- test/alloy.test.ts
+++ test/alloy.test.ts
@@ -1,20 +1,37 @@
-import { sendEvent } from '../src/alloy';
+import alloy from '@adobe/alloy';
+import { sendEvent, getInstance } from '../src/alloy';
 
-jest.mock("@adobe/alloy");
+jest.mock('@adobe/alloy', () => ({
+  createInstance: jest.fn(param => {
+    console.log(param);
+    return jest.fn();
+  }),
+}));
 
-// let alloySpy;
-// let windowSpy;
+let mockInstance: any;
 
-describe("Alloy", () => {
-    beforeEach(() => {
-        // const alloy = new Alloy();
-        // alloySpy = jest.spyOn(alloy, 'setInstance');
-    });
+describe('Alloy', () => {
+  beforeEach(() => {
+    mockInstance = getInstance();
+  });
 
-    test('alloy instance should exist', () => {
-        sendEvent({ foo: "bar" });
-        
-        // const instance = getInstance();
-        // expect(instance).toBe(2);
-    })
-})
+  /**
+   * importing `../src/alloy` should initialize the instance
+   * we might not want to do this, but it's how it's working right now
+   */
+  it('should initialize alloy on import', () => {
+    expect(alloy.createInstance).toBeCalledTimes(1);
+    expect(alloy.createInstance).toBeCalledWith({ name: 'alloy' });
+  });
+
+  /**
+   * check to see if the alloy instance is on window.alloy
+   */
+  it.skip('should exist on the window object', () => {})
+
+  it('should call the instance on sendEvent', () => {
+    sendEvent({ foo: 'bar' });
+
+    expect(mockInstance).toBeCalledTimes(1);
+  });
+});
